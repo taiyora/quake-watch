297811b5965e80f77012808f2e0328a6846a7874
jl-eiki <jl-eiki@protonmail.com>
2017-12-02 22:38:31 +1300

    Retrieves latest quakes and displays basic info

---
+++ src/quake-watch.py
@@ -0,0 +1,73 @@
+import json
+import requests
+import time
+from asciimatics.screen import Screen
+
+update_freq = 5 # minutes
+
+# Specify where each screen element should go
+ui = {
+    'screen': { 'w': 200, 'h': 105 },
+
+    'error': { 'x': 0, 'y': 0 },
+
+    'quake-list': { 'x':  0, 'y': 1 },
+    'locality'  : { 'x':  0 },
+    'magnitude' : { 'x': 40 }
+}
+
+def printError(screen, message):
+    screen.print_at(message, ui['error']['x'], ui['error']['y'], Screen.COLOUR_RED, Screen.A_BOLD)
+
+def getLatestQuakes(screen):
+    """Retrieves the latest 100 earthquakes from the GeoNet API."""
+
+    try:
+        quakes = requests.get('http://api.geonet.org.nz/quake?MMI=-1')
+        quakes.raise_for_status()
+
+    except requests.exceptions.HTTPError        as e: printError(screen, 'HTTP Error: '        + str(e))
+    except requests.exceptions.ConnectionError  as e: printError(screen, 'Connection Error: '  + str(e))
+    except requests.exceptions.Timeout          as e: printError(screen, 'Timeout: '           + str(e))
+    except requests.exceptions.RequestException as e: printError(screen, 'Request Exception: ' + str(e))
+
+    else:
+        # Reformat the retrieved earthquake data into a more easy to use and sortable structure
+        quakes_json = quakes.json()
+        quakes_formatted = {}
+
+        for quake in quakes_json['features']:
+            quakes_formatted[quake['properties']['publicID']] = quake # Earthquakes will be indexed by their Public ID
+        
+        return quakes_formatted
+
+    return {} # Return an empty dictionary if an error occurred, since the program will continue to run fine
+
+def printQuake(screen, quake, n):
+    y = ui['quake-list']['y'] + n
+
+    locality  =     quake['properties']['locality']
+    magnitude = str(quake['properties']['magnitude'])
+
+    screen.print_at(locality,  ui['locality'] ['x'], y)
+    screen.print_at(magnitude, ui['magnitude']['x'], y)
+
+def main(screen):
+    while True:
+        # Clear the screen
+        screen.move(0, 0)
+        screen.draw(ui['screen']['w'], ui['screen']['h'], char=' ')
+
+        # Display the latest earthquakes
+        quakes_latest = getLatestQuakes(screen)
+
+        n = 0
+        for q_id in quakes_latest:
+            printQuake(screen, quakes_latest[q_id], n)
+            n += 1
+
+        screen.refresh()
+        time.sleep(update_freq * 60)
+
+if __name__ == '__main__':
+    Screen.wrapper(main)
